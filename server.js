c4897697370d6a1f1f571f95ee256ae7b9679191
linxianliang <xianlianglin@sfmail.sf-express.com>
2019-09-30 15:52:34 +0800

    2019-09.md

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(async (ctx) => {
   spinner.start();
   shell.exec('git pull', (code, stdout, stderr) => {
     if (code !== 0) {
-      console.log(stderr);
+      spinner.text = stderr;
       spinner.fail("拉取失败，请手动更新代码！");
       shell.exit(1);
     } else {
+      spinner.text = stdout;
       spinner.succeed("拉取成功！");
       const fileName = `${dayjs().format('YYYY-MM')}.md`;
       const fileContent = `[${content.title}](${content.link})</br></br>`;
@@ -33,10 +34,11 @@ app.use(async (ctx) => {
           spinner.start();
           shell.exec(`git add . && git commit -m ${fileName} && git push -u origin master`, (multiCode, multiStdout, multiErr) => {
             if (multiCode !== 0) {
-              console.log(multiErr)
+              spinner.text = multiErr;
               spinner.fail("同步失败！");
               shell.exit(1);
             } else {
+              spinner.text = multiStdout;
               spinner.succeed("已完成同步！");
             }
           })
@@ -45,47 +47,6 @@ app.use(async (ctx) => {
     }
   })
   ctx.body = content;
-
-
-
-
-  // shell.exec('git pull', (e, s) => {
-  //   console.log(111)
-  //   spinner.start();
-  //   spinner.text = '正在拉取GitHub上的变动！'
-  //   if (e) {
-  //     console.log(222)
-  //     console.log(e)
-  //     spinner.fail("拉取失败，请手动更新代码！");
-  //     return false;
-  //   } else {
-  //     console.log(333)
-  //     spinner.succeed("拉取成功！");
-  //     const fileName = `${dayjs().format('YYYY-MM')}.md`;
-  //     const fileContent = `[${content.title}](${content.link})</br></br>`;
-  //     fs.exists('bookmarks', (exists) => {
-  //       !exists && fs.mkdirSync('bookmarks');
-  //     })
-  //     fs.appendFile(`bookmarks\\${fileName}`, fileContent, (error) => {
-  //       error && ctx.throw(error);
-  //       console.log('写入成功，正在同步至GitHub！');
-  //       spinner.start();
-  //       spinner.text = '正在同步中，请勿中断进程！'
-  //       let execGit = exec(`git add . && git commit -m ${fileName} && git push -u origin master`, (err, stdout) => {
-  //         console.log(444)
-  //         if (err) {
-  //           console.log(err)
-  //           spinner.fail("同步失败！");
-  //         } else {
-  //           console.log(stdout)
-  //           spinner.succeed("已完成同步！");
-  //         }
-  //         execGit.kill();
-  //       })
-  //     })
-  //   }
-  // })
-
 })
 
 app.listen(3000, () => {
